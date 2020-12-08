dee386cb5c9f9f60064f7259d5160314677a5e50
Buges Killian <killian.buges@gmail.com>
2020-12-08 17:55:33 +0100

    change on auth

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,30 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setLogin('lucas@ld-web.net')
+            ->setRoles([UserRole::ADMIN]);
+
+        $user->setPassword($this->encoder->encodePassword(
+            $user,
+            'blablabla'
+        ));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('test@test.com');
+        $user->setPassword($this->encoder->encodePassword(
+            $user,
+            'blablabla'
+        ));
+
+        $manager->persist($user);
 
         $manager->flush();
     }
